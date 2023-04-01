d00827b4e5803e01db31b8bad5bd96dbfe48ef92
Arttu Pönni <arttu.ponni@intito.fi>
2023-04-01 11:00:31 +0300

    is-interactive block, atuin added

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -8,26 +8,30 @@ set -gx EDITOR nvim
 set -U fish_user_paths /home/arttu/bin $fish_user_paths
 set -U fish_user_paths /home/arttu/.local/bin $fish_user_paths
 
-# Aliases
-alias vim nvim
-alias v nvim
-alias r ranger
-alias ls exa
-alias la "exa -al"
-alias cat bat
-alias top btop
-alias lab jupyter-lab
+if status is-interactive
+    # Aliases
+    alias vim nvim
+    alias v nvim
+    alias r ranger
+    alias ls exa
+    alias la "exa -al"
+    alias cat bat
+    alias top btop
+    alias lab jupyter-lab
 
-# Starship shell
-starship init fish | source
+    # Starship shell
+    starship init fish | source
 
-# Hook for direnv
-direnv hook fish | source
+    # Hook for direnv
+    direnv hook fish | source
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/arttu/.miniconda3/bin/conda
-    eval /home/arttu/.miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    # Atuin shell history
+    atuin init fish --disable-up-arrow | source
+
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    if test -f /home/arttu/.miniconda3/bin/conda
+        eval /home/arttu/.miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    end
+    # <<< conda initialize <<<
 end
-# <<< conda initialize <<<
-
